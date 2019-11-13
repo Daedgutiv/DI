5333bbd2a225827140703abdbcfc9b9cd45c5563
Daedgutiv <daavogutis@gmail.com>
2019-11-13 19:15:18 +0100

    Finalizado

--- P1/team-builder/src/app/mock-players.ts
+++ P1/team-builder/src/app/mock-players.ts
@@ -11,5 +11,15 @@ export const PLAYERS: Player[] = [
     {id: 18, name: 'Jaric', dorsal: 3, position:'Defensa' },
     {id: 19, name: 'Valeny', dorsal: 4, position:'Defensa' },
     {id: 20, name: 'Stein', dorsal: 5, position:'Medio' },
+    {id: 21, name: 'Castolo', dorsal: 9, position:'Delantero' },
+    {id: 22, name: 'Espimas', dorsal: 8, position:'Medio' },
+    {id: 23, name: 'Ordaz', dorsal: 11, position:'Delantero' },
+    {id: 24, name: 'Minanda', dorsal: 10, position:'Medio' },
+    {id: 25, name: 'Ivarov', dorsal: 1, position:'Portero' },
+    {id: 26, name: 'Stremer', dorsal: 2, position:'Defensa' },
+    {id: 27, name: 'Ximelez', dorsal: 7, position:'Medio' },
+    {id: 28, name: 'Jaric', dorsal: 3, position:'Defensa' },
+    {id: 29, name: 'Valeny', dorsal: 4, position:'Defensa' },
+    {id: 30, name: 'Stein', dorsal: 5, position:'Medio' },
 
 ]
